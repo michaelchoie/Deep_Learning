daff3659c58792f8ea58644ded543689c6d93ad2
Michael Choie <MichaelChoie@Michaels-MacBook-Pro-2.local>
2018-02-06 16:43:39 -0500

    Changed syntax and file system hierarchy

--- generate_tv_script/README.md
+++ generate_tv_script/README.md
@@ -4,7 +4,7 @@
 By using machine learning techniques such as word2vec and seq2seq, we can teach a recurrent neural network how to write a TV script by training it on sample Simpsons text. The following modules prepare and process our text data.
 
 ## Key Files
-preprocess.py - in charge of creating an embedding layer, punctuation tokenization, functions to handle pickle files, and batches for the learning/optimization process
+preprocess.py - in charge of creating an embedding layer, punctuation tokenization, functions to handle pickle files
 script_generator.py - in charge of creating the RNN, training the network, and producing a script
 test_file.py - in charge of testing functions in the modules above
 
--- generate_tv_script/src/preprocess.py
+++ generate_tv_script/src/preprocess.py
@@ -1,118 +1,116 @@
-import numpy as np
-import os 
-import pickle 
+"""Prepare tv script text data so that it can be inputted into a neural net."""
+
+import pickle
 from collections import Counter
-
-def load_data(path):
-	"""
-	Load data from file
-	"""
-
-	file = os.path.join(path)
-	with open(file, "r") as f:
-		data = f.read()
-
-	return data
-
-def token_lookup():
-	"""
-	Generate a dictionary to turn punctuation into a token.
-	:return: Tokenize dictionary where the key is the punctuation and the value is the token
-	"""
-
-	token_list = {
-				 ".": "||Period||",
-				 ",": "||Comma||",
-				 '"': "|Quotation_Mark||",
-				 ";": "||Semicolon||",
-				 "!": "||Exclamation_Mark||",
-				 "?": "||Question_Mark||",
-				 "(": "||Left_Parentheses||",
-				 ")": "||Right_Parentheses||",
-				 "--": "||Dash||",
-				 "\n": "||Return||"
-				 }
-
-	return token_list
-
-def create_lookup_tables(text):
-	"""
-	Create lookup tables for vocabulary
-	:param text: The text of tv scripts split into words
-	:return: A tuple of dicts (vocab_to_int, int_to_vocab)
-	"""
-	word_counts = Counter(text)
-	sorted_words = sorted(word_counts, key = word_counts.get, reverse = True)
-	vocab_to_int = {word: ii for ii, word in enumerate(sorted_words)}
-	int_to_vocab = {ii: word for ii, word in enumerate(sorted_words)}
-	return (vocab_to_int, int_to_vocab)
-
-def preprocess_and_save_data(dataset_path, token_lookup, create_lookup_tables):
-	"""
-	Preprocess text data
-	:params dataset_path: path to data
-	:params token_lookup: function
-	:params create_lookup_tables: function
-	"""
-
-	# File starts with "[YEAR DATE 1989] © Twentieth Century Fox Film Corporation. All rights reserved."
-	# Irrelevant information so remove
-	text = load_data(dataset_path)
-	text = text[81:]
-	
-	# Replace punctuation as tokens to make differentiating words and sentiment easier
-	token_dict = token_lookup()
-	for key, value in token_dict.items():
-		text = text.replace(key, ' {} '.format(value))
-
-	# Transform text for use in lookup tables
-	text = text.lower()
-	text = text.split()
-
-	# Create lookup tables
-	vocab_to_int, int_to_vocab = create_lookup_tables(text)
-	int_text = [vocab_to_int[word] for word in text]
-
-	pickle.dump((int_text, vocab_to_int, int_to_vocab, token_dict), open("preprocess.p", "wb"))
-
-def load_preprocess():
-	"""
-	Load the Preprocessed Training data and return them in batches of <batch_size> or less	
-	"""
-
-	return pickle.load(open("preprocess.p", "rb"))
+from os import getcwd
+from os.path import abspath, join
 
 
-def save_params(params):
-	"""
-	Save parameters to file
-	"""
+class DataPreprocessor(object):
+    """
+    Preprocess text data for RNN input.
 
-	pickle.dump(params, open("params.p", "wb"))
+    Contains functions that load data, tokenize punctuation,
+    create embedding layer, and handles pickle files
 
-def load_params():
-	"""
-	Load parameters from file
-	"""
+    Attributes:
+        file_name (str): name of txt file
+        path_to_file (str): path to txt file
+    """
 
-	return pickle.load(open("params.p", "rb"))
+    def __init__(self):
+        """Initialize object with info on .txt file and path."""
+        self.file_name = 'moes_tavern_lines.txt'
+        self.path_to_file = abspath(join(getcwd(), '../data',
+                                         self.file_name))
 
-def get_batches(int_text, batch_size, seq_length):
-	"""
-	Return batches of input and target
-	:param int_text: Text with the words replaced by their ids
-	:param batch_size: The size of batch
-	:param seq_length: The length of sequence
-	:return: Batches as a Numpy array
-	"""
+    def _load_data(self, path):
+        """
+        Load data from file.
 
-	n_sequences = len(int_text) // (batch_size * seq_length) 
-	n_words = n_sequences * batch_size * seq_length
-	inputs = np.array(int_text[:n_words])
+        Return:
+            data (str): text data
+        """
+        with open(self.path_to_file, "r") as f:
+            data = f.read()
 
-	input_batches = np.split(inputs.reshape(batch_size, -1), n_sequences, axis = 1)
-	targets = np.array(int_text[1:n_words + 1])
-	targets[-1] = int_text[0]
-	target_batches = np.split(targets.reshape(batch_size, -1), n_sequences, axis = 1)
+        return data
 
-	return np.array(list(zip(input_batches, target_batches)))
+    def _create_lookup_tables(self, text):
+        """
+        Create lookup tables for vocabulary.
+
+        Args:
+            text (str): The text of tv scripts split into words
+        Return:
+            tuple (vocab_to_int, int_to_vocab)
+        """
+        word_counts = Counter(text)
+        sorted_words = sorted(word_counts, key=word_counts.get, reverse=True)
+        vocab_to_int = {word: ii for ii, word in enumerate(sorted_words)}
+        int_to_vocab = {ii: word for ii, word in enumerate(sorted_words)}
+        return (vocab_to_int, int_to_vocab)
+
+    @staticmethod
+    def token_lookup():
+        """
+        Generate a dictionary to turn punctuation into tokens.
+
+        Return:
+            token_list (dict): key is symbol, value is token
+        """
+        token_list = {".": "||Period||",
+                      ",": "||Comma||",
+                      '"': "|Quotation_Mark||",
+                      ";": "||Semicolon||",
+                      "!": "||Exclamation_Mark||",
+                      "?": "||Question_Mark||",
+                      "(": "||Left_Parentheses||",
+                      ")": "||Right_Parentheses||",
+                      "--": "||Dash||",
+                      "\n": "||Return||"
+                      }
+
+        return token_list
+
+    def preprocess_and_save_data(self, dataset_path):
+        """
+        Preprocess and save text data.
+
+        Args:
+            dataset_path (str): path to data
+        """
+        # File starts with copyright line - irrelevant information so remove
+        text = self._load_data(dataset_path)
+        text = text[81:]
+
+        # Replace punctuation as tokens for text processing
+        token_dict = self.token_lookup()
+        for key, value in token_dict.items():
+            text = text.replace(key, ' {} '.format(value))
+
+        # Transform text for use in lookup tables
+        text = text.lower()
+        text = text.split()
+
+        # Create lookup tables
+        vocab_to_int, int_to_vocab = self._create_lookup_tables(text)
+        int_text = [vocab_to_int[word] for word in text]
+
+        pickle.dump((int_text, vocab_to_int, int_to_vocab, token_dict),
+                    open("preprocess.p", "wb"))
+
+    def load_preprocess(self):
+        """Load the preprocessed training data and return it."""
+        return pickle.load(open("preprocess.p", "rb"))
+
+
+def main():
+    """Initialize data preprocessor object and save preprocessed data."""
+    p = DataPreprocessor()
+    p.preprocess_and_save_data(p.path_to_file, p.token_lookup,
+                               p._create_lookup_tables)
+
+
+if __name__ == "__main__":
+    main()
